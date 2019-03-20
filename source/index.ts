1336268639c3727900ae1f06bd26f1d3dfb8658a
Simon Mika <simon@mika.se>
2019-03-20 14:29:30 +0100

    Added language support.

--- source/index.ts
+++ source/index.ts
@@ -3,6 +3,7 @@ import { Currency } from "./Currency"
 import { CurrencyCode } from "./CurrencyCode"
 import { Error } from "./Error"
 import * as hal from "./hal"
+import { Language } from "./Language"
 
 export {
 	Connection,
@@ -10,4 +11,5 @@ export {
 	CurrencyCode,
 	Error,
 	hal,
+	Language,
 }
