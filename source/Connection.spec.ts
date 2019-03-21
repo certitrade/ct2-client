37d4ed554056c2b65ab8dc527a473c292f7504fd
Simon Mika <simon@mika.se>
2019-03-21 06:10:50 +0100

    Fixed imports.

--- source/Connection.spec.ts
+++ source/Connection.spec.ts
@@ -1,7 +1,5 @@
 import { Connection } from "./Connection"
-import { Error } from "./Error"
-import * as hal from "./hal"
-
+import * as model from "@certitrade/ct2-model"
 import "../environment.local"
 
 describe("Connection", () => {
@@ -9,23 +7,23 @@ describe("Connection", () => {
 	it("CertitradeUserId environment variable set", () => expect(process.env.CertitradeUserId).toBeTruthy())
 	it("CertitradeUserKey environment variable set", () => expect(process.env.CertitradeUserKey).toBeTruthy())
 	it("new", () => {
-		const connection = new Connection(process.env.CertitradeBaseUrl, "12345", "0123456789abcdefghij")
+		const connection = new Connection(process.env.CertitradeBaseUrl || "", "12345", "0123456789abcdefghij")
 		expect(connection).toBeTruthy()
 		expect(connection.baseUrl).toBe(process.env.CertitradeBaseUrl)
 	})
 	it("not authorized", async () => {
-		const connection = new Connection(process.env.CertitradeBaseUrl, "12345", "0123456789abcdefghij")
+		const connection = new Connection(process.env.CertitradeBaseUrl || "", "12345", "0123456789abcdefghij")
 		expect(connection).toBeTruthy()
-		const answer = await connection.get("payment") as Error
+		const answer = await connection.get("payment") as model.Error
 		expect(answer.describedBy).toBeNull()
 		expect(answer.title).toBe("Not authorized")
 		expect(answer.httpStatus).toBe(403)
 		expect(answer.detail).toBe("")
 	})
 	it("authorized", async () => {
-		const connection = new Connection(process.env.CertitradeBaseUrl, process.env.CertitradeUserId || "", process.env.CertitradeUserKey || "")
+		const connection = new Connection(process.env.CertitradeBaseUrl || "", process.env.CertitradeUserId || "", process.env.CertitradeUserKey || "")
 		expect(connection).toBeTruthy()
-		const payments = await connection.get("payment") as hal.Collection
+		const payments = await connection.get("payment") as model.hal.Collection
 		expect(payments).toHaveProperty("total_size")
 		expect(payments).toHaveProperty("max_page_size")
 	})
--- source/Connection.ts
+++ source/Connection.ts
@@ -1,11 +1,11 @@
 import * as crypto from "crypto"
-import { Error } from "./Error"
+import * as model from "@certitrade/ct2-model"
 import { default as fetch, RequestInit } from "node-fetch"
 
 export class Connection {
 	constructor(readonly baseUrl: string, private userID: string, private userKey: string) {
 	}
-	private async fetch<T>(resource: string, init: RequestInit, body?: any): Promise<T | Error> {
+	private async fetch<T>(resource: string, init: RequestInit, body?: any): Promise<T | model.Error> {
 		const url = this.baseUrl + resource
 		const date = new Date().toUTCString()
 		if (body)
@@ -19,33 +19,33 @@ export class Connection {
 				date,
 			},
 		})
-		let result: T | Error
+		let result: T | model.Error
 		switch (response.headers.get("content-type")) {
 			case "application/hal+json":
 				result = await response.json() as T
 				break
 			case "application/api-problem+json":
-				result = await response.json() as Error
+				result = await response.json() as model.Error
 				break
 			default:
-				result = { describedBy: this.baseUrl + resource, title: "Connection Error", httpStatus: response.status, detail: await response.text() }
+				result = { describedBy: this.baseUrl + resource, title: "Connection model.Error", httpStatus: response.status, detail: await response.text() }
 				break
 		}
 		return result
 	}
-	get<T>(resource: string): Promise<T | Error> {
+	get<T>(resource: string): Promise<T | model.Error> {
 		return this.fetch<T>(resource, { method: "GET" })
 	}
-	post<T>(resource: string, body: any): Promise<T | Error> {
+	post<T>(resource: string, body: any): Promise<T | model.Error> {
 		return this.fetch<T>(resource, { method: "POST" }, body)
 	}
-	put<T>(resource: string, body: any): Promise<T | Error> {
+	put<T>(resource: string, body: any): Promise<T | model.Error> {
 		return this.fetch<T>(resource, { method: "PUT" }, body)
 	}
-	delete<T>(resource: string): Promise<T | Error> {
+	delete<T>(resource: string): Promise<T | model.Error> {
 		return this.fetch(resource, { method: "DELETE" })
 	}
-	options<T>(resource: string): Promise<T | Error> {
+	options<T>(resource: string): Promise<T | model.Error> {
 		return this.fetch(resource, { method: "OPTIONS" })
 	}
 }
