4d6f16b49d35d26f2b88bc6f56a324f47812a851
Simon Mika <simonmika@users.noreply.github.com>
2019-04-25 19:31:12 +0200

    Folder structure (#3)
    * Updated dependency.
    
    * Rearranged folder structure.

--- Connection.spec.ts
+++ Connection.spec.ts
@@ -1,6 +1,6 @@
-import { Connection } from "./Connection"
-import * as model from "@certitrade/ct2-model"
 import * as dotenv from "dotenv"
+import * as model from "@certitrade/ct2-model"
+import * as client from "."
 
 dotenv.config()
 
@@ -9,12 +9,12 @@ describe("Connection", () => {
 	it("pspUserId environment variable set", () => expect(process.env.pspUserId).toBeTruthy())
 	it("pspUserKey environment variable set", () => expect(process.env.pspUserKey).toBeTruthy())
 	it("new", () => {
-		const connection = new Connection(process.env.pspBaseUrl || "", "12345", "0123456789abcdefghij")
+		const connection = new client.Connection(process.env.pspBaseUrl || "", "12345", "0123456789abcdefghij")
 		expect(connection).toBeTruthy()
 		expect(connection.baseUrl).toBe(process.env.pspBaseUrl)
 	})
 	it("not authorized", async () => {
-		const connection = new Connection(process.env.pspBaseUrl || "", "12345", "0123456789abcdefghij")
+		const connection = new client.Connection(process.env.pspBaseUrl || "", "12345", "0123456789abcdefghij")
 		expect(connection).toBeTruthy()
 		const answer = await connection.get("payment") as model.Error
 		expect(answer.describedBy).toBeNull()
@@ -23,9 +23,9 @@ describe("Connection", () => {
 		expect(answer.detail).toBe("")
 	})
 	it("authorized", async () => {
-		const connection = new Connection(process.env.pspBaseUrl || "", process.env.pspUserId || "", process.env.pspUserKey || "")
+		const connection = new client.Connection(process.env.pspBaseUrl || "", process.env.pspUserId || "", process.env.pspUserKey || "")
 		expect(connection).toBeTruthy()
-		const payments = await connection.get("payment") as model.hal.Collection
+		const payments = await connection.get("payment")
 		expect(payments).toHaveProperty("total_size")
 		expect(payments).toHaveProperty("max_page_size")
 	})
